aacdafa12761e278a5a71dd0a936c0fb3cc930e5
yuxiu-chen <yuxiu_chen@mayohr.com>
2024-10-30 14:12:25 +0800

    Update replace-title.js

--- replace-title.js
+++ replace-title.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         改 Title
 // @namespace    http://tampermonkey.net/
-// @version      2024-10-23
+// @version      2024-10-30
 // @description  try to take over the world!
 // @author       Yuxiu
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mayohr.com
@@ -14,8 +14,6 @@
 // @match        https://*.mayohr.com/payroll*
 // ==/UserScript==
 
-
-
 function setNewTitle() {
     var menu = "li.apollo-sidebar-navigation-item.active div.apollo-sidebar-navigation-text";
 
@@ -27,7 +25,6 @@ function setNewTitle() {
         menu = "li.sc-bXCLTC.etuLpc.active div.sc-hmdomO.gACZpk"
     }
 
-    console.log("yuxiu",menu, document.querySelector(menu))
     var newTitle = document.querySelector(menu).innerHTML;
 
     newTitle = newTitle.replace(/<span[^>]*>/g,'').replace(/<\/span>/g,'').replace(/<\!--.*?-->/g, "");
@@ -35,22 +32,7 @@ function setNewTitle() {
     document.title = newTitle;
 }
 
-function runSetNewTitleMultiTimes() {
-    // 四次是因為有時候頁面載太慢了
-    setTimeout(function() { setNewTitle(); }, 1000);
-    setTimeout(function() { setNewTitle(); }, 1500);
-    setTimeout(function() { setNewTitle(); }, 2000);
-    setTimeout(function() { setNewTitle(); }, 2500);
-}
-
 (function() {
     'use strict';
-    // 載入時修改 title
-   runSetNewTitleMultiTimes();
-
-    // 點擊 menu 後修改 title
-    setTimeout(function() {document.querySelector("ul.apollo-sidebar-navigation").addEventListener("click", (e) => {runSetNewTitleMultiTimes();});}, 1000);
-    setTimeout(function() {document.querySelector("ul.apollo-sidebar-navigation").addEventListener("click", (e) => {runSetNewTitleMultiTimes();});}, 1500);
-    setTimeout(function() {document.querySelector("ul.apollo-sidebar-navigation").addEventListener("click", (e) => {runSetNewTitleMultiTimes();});}, 2000);
-    setTimeout(function() {document.querySelector("ul.apollo-sidebar-navigation").addEventListener("click", (e) => {runSetNewTitleMultiTimes();});}, 2500);
+    setInterval(setNewTitle, 1000)
 })();
